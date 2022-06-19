62e43c4c1dc55a445eb59088d19f3f494f5eba8c
FikriDean <fkrdean@gmail.com>
2022-06-19 17:55:11 +0700

    sa

--- index.js
+++ index.js
@@ -11,16 +11,15 @@ const db = require('./models')
 const postsRouter = require('./routes/posts');
 
 const commentsRouter = require('./routes/comments');
-
 const usersRouter = require('./routes/users');
 app.use("/posts", postsRouter);
 app.use('/comments', commentsRouter);
 app.use('/auth', usersRouter);
 
-const port = process.env.PORT || 3001;
+const PORT = 3001;
 
 db.sequelize.sync().then(() => {
-  app.listen(port, () => {
+  app.listen(process.env.PORT || PORT, () => {
     console.log('Server is running on port 3001');
   })
 })
